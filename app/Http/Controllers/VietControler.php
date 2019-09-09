94f0a5d689742d41ce1d429e23f5c99fa822276f
vietawake <vietawake@gmail.com>
2019-09-09 21:14:53 +0700

    first commit

---
+++ app/Http/Controllers/VietControler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VietControler extends Controller
+{
+    public function abc() {
+        echo 'Hi, My name is Viet, Nice to meet you';
+    }
+}
